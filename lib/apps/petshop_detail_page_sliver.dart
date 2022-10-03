3e1834047d59d5dddfb9aa6ac35768b8ce55a5c1
Guilherme Carneiro <guilherme.carvalho@palmas.to.gov.br>
2022-10-03 17:59:41 -0300

    feat: :sparkles: Trabalhando na seleção de serviços

--- lib/apps/petshop_detail_page_sliver.dart
+++ lib/apps/petshop_detail_page_sliver.dart
@@ -2,6 +2,7 @@ import 'package:design_pinterest/core/mock_data/mock_data.dart';
 import 'package:flutter/material.dart';
 
 import '../core/colors.dart';
+import '../core/nuvols_logger.dart';
 import '../core/widgets/petshop_detail_page_head.dart';
 import '../core/widgets/petshop_listservices.dart';
 import '../core/widgets/petshop_white_container.dart';
@@ -15,33 +16,76 @@ class PetShopDetailPageSliver extends StatefulWidget {
 }
 
 class _PetShopDetailPageSliverState extends State<PetShopDetailPageSliver> {
+  List<String> servicesChoice = <String>[];
+  final servicesNotifier = ValueNotifier(0);
+
+  void _serviceAddRemove(String serviceID) {
+    try {
+      if (servicesChoice.contains(serviceID)) {
+        servicesChoice.remove(servicesChoice.where((element) => element == serviceID).first);
+      } else {
+        servicesChoice.add(serviceID);
+      }
+      servicesNotifier.value = servicesChoice.length;
+    } catch (error, stackTrace) {
+      NuvolsLogger().erro("-----ERRO-----", error, stackTrace);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          stretch: true,
-          floating: true,
-          pinned: true,
-          snap: false,
-          expandedHeight: 250,
-          backgroundColor: CustomColors.yellowFinalBackground,
-          title: Text(
-            NuvolsCoreMockData.gerarPalavra(),
-          ),
-          flexibleSpace: const FlexibleSpaceBar(
-            background: PetShopHead(),
-            stretchModes: <StretchMode>[
-              StretchMode.zoomBackground,
-              StretchMode.blurBackground,
-              StretchMode.fadeTitle,
+    NuvolsLogger().debug("Build da App");
+    return Container(
+      padding: const EdgeInsets.only(top: kToolbarHeight),
+      decoration: BoxDecoration(gradient: CustomBackgroundColors.gradientAppBarPetshop),
+      child: Stack(
+        children: [
+          CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                stretch: true,
+                floating: true,
+                pinned: true,
+                snap: false,
+                expandedHeight: 235,
+                backgroundColor: CustomColors.yellowInitialBackground.withOpacity(0.85),
+                title: Text(
+                  NuvolsCoreMockData.gerarPalavra(),
+                ),
+                flexibleSpace: const FlexibleSpaceBar(
+                  background: PetShopHead(),
+                  stretchModes: <StretchMode>[
+                    StretchMode.zoomBackground,
+                    StretchMode.blurBackground,
+                    StretchMode.fadeTitle,
+                  ],
+                ),
+              ),
+              const PetShopWhiteCardOverHeader(),
+              PetShopListServices(addRemoveService: _serviceAddRemove),
             ],
           ),
-        ),
-        const PetShopWhiteCardOverHeader(),
-        const PetShopListServices(),
-      ],
-    ));
+          ValueListenableBuilder<int>(
+            valueListenable: servicesNotifier,
+            builder: (BuildContext _, dynamic value, Widget? child) {
+              return Visibility(
+                  visible: value != 0,
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      height: 85,
+                      width: MediaQuery.of(context).size.width * .85,
+                      color: Colors.amberAccent,
+                      child: Text(
+                        "${value.toString()} Serviços selecionados",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ));
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
